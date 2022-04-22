2ce466e52e70b12806480b4b347949e66d0a40d3
phoridFly <46564583+phoridFly@users.noreply.github.com>
2022-04-21 20:10:41 -0700

    get specimen by id

--- flies/db_functions/functions_specimens.js
+++ flies/db_functions/functions_specimens.js
@@ -16,6 +16,51 @@ module.exports =
      * Function getAllSpecimens returns all the specimen information in the database
      * @returns [array]       Array of specimen objects
      */
+    getSpecimenById : getSpecimenById = (req) =>
+    {
+        let specimenQuery = `
+        SELECT 
+            specimen.id,
+            specimen.institution_code,
+            specimen.collection_code,
+            specimen.catalog_number,
+            specimen.species_id,
+            species.genus,
+            species.specific_epithet,
+            specimen.collecting_event_id,
+            locality.country,
+            locality.place,
+            DATE_FORMAT(collecting_event.start_date, "%b %d %Y") AS start_date,
+            DATE_FORMAT(collecting_event.end_date, "%b %d %Y") AS end_date,
+            collecting_event.method,
+            specimen.type_status,
+            specimen.sex,
+            specimen.image_name 
+        FROM 
+            specimen
+        INNER JOIN 
+            collecting_event ON specimen.collecting_event_id = collecting_event.id
+        INNER JOIN 
+            locality ON collecting_event.locality_id = locality.id
+        INNER JOIN 
+            species ON specimen.species_id = species.id
+        WHERE
+            specimen.id = ?
+        `;
+
+        return new Promise((resolve, reject) => {
+            mysql.pool.query(specimenQuery, [req.query.id], (error, elements) => {
+                if(error) {
+                    return reject(error);
+                }
+                return resolve(elements);
+            }); 
+        });
+    },
+        /**
+     * Function getAllSpecimens returns all the specimen information in the database
+     * @returns [array]       Array of specimen objects
+     */
     // TODO Paginate this. It could get HUUUUUGE 
     getAllSpecimens : getAllSpecimens = (req) =>
     {
--- flies/routes/routes_specimens.js
+++ flies/routes/routes_specimens.js
@@ -3,12 +3,16 @@ const router = require('express').Router();
 const 
 {
     getAllSpecimens,
+    getSpecimenById
 } = require('../db_functions/functions_specimens');
 
 router.get('/', async (req, res, next) => {
 
-    const out = await getAllSpecimens(req); 
-    
+    const out = {};
+    //console.log(req.query);
+    if (req.query.id) { out.specimenDetails = await getSpecimenById(req); } 
+    else { out.specimens = await getAllSpecimens(req); } 
+
     res.send(out);
     
 });
